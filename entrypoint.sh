47e85a6360e62bc51fa9aee3a00cb06c11818bce
Tomasz Kulig <pewienjaaa@gmail.com>
2025-06-04 10:29:22 +0200

    Fix docker for production and development (#18)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-echo 'Waiting for postgres...'
 
-while ! nc -z $DB_HOST $DB_PORT; do
-    sleep 0.1
-done
-
+if [ "$RUN_MIGRATIONS" = "true" ]; then
 echo 'Running migrations...'
 python manage.py migrate
 python manage.py loaddata api/fixtures/collections.json
+fi
 
 exec "$@"
